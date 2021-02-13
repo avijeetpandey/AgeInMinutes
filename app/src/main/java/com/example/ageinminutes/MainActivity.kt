0bfeb6936606a72ff99f552f419b623184013485
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-02-13 21:31:39 +0530

    Add : Open date picker functionality

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // grab the select date button by ID
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener{view->
+            clickDatePicker(view)
+        }
+
+    }
+
+    fun clickDatePicker(view:View){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    Toast.makeText(this,"Date Picker works",Toast.LENGTH_LONG).show()
+        },year,month,day).show()
     }
 }
